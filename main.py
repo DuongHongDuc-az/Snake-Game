bd186dcaae4ca31542691cde2a3f4b011822664a
DuongHongDuc-az <hongducduong.tphcm@gmail.com>
2025-11-05 19:04:39 +0700

    Initial commit - Snake Game project setup

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((600, 400))
+pygame.display.set_caption("Snake Game")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+pygame.quit()
