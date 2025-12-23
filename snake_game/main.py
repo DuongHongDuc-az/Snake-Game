542035dd809c4064fd6afdfdda042d9d4c77f02a
DuongHongDuc-az <hongducduong.tphcm@gmail.com>
2025-12-23 20:10:49 +0700

    Add README.md

--- snake_game/main.py
+++ snake_game/main.py
@@ -3,12 +3,6 @@ import torch
 import sys
 from snake.app import Game
 from snake.scenes.intro import UI_Manager
-
-# if __name__ == "__main__":
-#    Game().run()a
-#    pygame.quit()
-#    sys.exit()
-
 if __name__ == "__main__":
    
  manager = UI_Manager()
